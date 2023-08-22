dd6e3abd9c8325b0eaae7242b32203a6a69c52d8
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-22 19:06:03 +0300

    Update conference_badges.py

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name)
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badge = badge_maker(name)
+        badges.append(badge)
+    return badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    for index, name in enumerate(names, start=1):
+        assignment = "Hello, {}! You'll be assigned to room {}!".format(name, index)
+        room_assignments.append(assignment)
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    assignments = assign_rooms(names)
+    
+    for badge in badges:
+        print(badge)
+    for assignment in assignments:
+        print(assignment)
+
